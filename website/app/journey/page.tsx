cb7cdf980424d6f78179e40e0f6f3fc0bf6ee815
Andy Rae <1127507+AndyRae@users.noreply.github.com>
2026-01-25 21:14:58 +0000

    Add timeline

--- website/app/journey/page.tsx
+++ website/app/journey/page.tsx
@@ -1,10 +1,18 @@
-
+import { Journey } from "@/components/core/journey";
 
 export default function Page() {
   return (
-    <div className="">
-      <h1>Journey</h1>
-      
+    <div className="w-full relative bg-brand">
+      <div className="absolute inset-0 flex items-center justify-start z-10">
+        <div className="px-6 lg:px-8 w-full">
+          <div className="mx-auto max-w-5xl">
+
+              <h1>Journey</h1>
+              <Journey />
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
